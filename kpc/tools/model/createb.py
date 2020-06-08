842ee1e0052535d0074c092bc25ebd91304642c6
pweekendwarrior <pweekendwarrior@gmail.com>
2020-06-07 21:28:11 -0400

    added some more models and recipes

--- kpc/tools/model/createb.py
+++ kpc/tools/model/createb.py
@@ -1,8 +1,12 @@
 import os
 import codecs
 itemName = input("item name: ").strip()
+
 text = """{
-	"parent": "kitchenparkour:block/""" + itemName + "\"" + """
-}"""
+	"parent": "block/cube_all",
+	"textures": {
+		"all": "kitchenparkour:block/custom_block_name"
+	}
+}""".replace("custom_block_name", itemName)
 with codecs.open(itemName + ".json", 'w+', encoding='utf8') as f:
     f.write(text)
